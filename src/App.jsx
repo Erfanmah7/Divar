66d6ec06dd07dac8cd31fe2745457394412dbdc6
Erfanmah7 <efn8mah@yahoo.com>
2024-08-04 22:36:19 +0330

    add react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { defaultOptions } from "./configs/reactQuery";
 
 function App() {
+  const queryClient = new QueryClient(defaultOptions);
+
   return (
     <BrowserRouter>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
---
+++ src/configs/reactQuery.js
@@ -0,0 +1,8 @@
+export const defaultOptions = {
+  queries: {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    staleTime: 6000,
+    retry: 1,
+  },
+};
