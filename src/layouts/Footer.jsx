60788970f3481c35e85787b9437d4ac9f13bdca8
Erfanmah7 <efn8mah@yahoo.com>
2024-08-09 13:32:58 +0330

    add layout with styles

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -4,7 +4,7 @@ import styles from "./footer.module.css";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>توسعه داده شده توسط عرفان ♥️</p>
+      <p>♥️ توسعه داده شده توسط عرفان</p>
     </footer>
   );
 }
