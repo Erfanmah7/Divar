c033f935113fca704d2c2ede1568d236be7f98df
Erfanmah7 <efn8mah@yahoo.com>
2024-08-10 18:55:17 +0330

    add sidebar with styles & favicon & title

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Sidebar from "../components/templates/Sidebar";
+import Main from "../components/templates/Main";
+
+const style = { display: "flex" };
 
 function HomePages() {
-  return <div>HomePage</div>;
+  return (
+    <div style={style}>
+      <Sidebar />
+      <Main />
+    </div>
+  );
 }
 
 export default HomePages;
